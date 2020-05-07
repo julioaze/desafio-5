bb9c70f61112ae87fba7da3f27b970fd1ba4336b
julioaze <julioaze@gmail.com>
2020-05-07 11:46:57 -0300

    desafio finalizado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,27 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public execute({ title, value, type }: Request): Transaction {
     // TODO
+    // extra: validar o tipo de transação
+    if(!['income', 'outcome'].includes(type)){
+      throw new Error('O tipo de operação é inválido.');
+    }
+
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if(type === 'outcome' && total < value) {
+      throw new Error('Não há saldo suficiente para a operação');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type
+    });
+
+    return transaction;
   }
 }
 
